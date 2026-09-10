7ff25382e92f8b15bd061ee3da8b7b14e62ff741
Yan Malinin <yanmalinin@gmail.com>
2026-09-10 19:03:16 +0500

    feat(cli): add command-line argument parsing

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-const js = require('@eslint/js');
-const globals = require('globals');
+import js from '@eslint/js';
+import globals from 'globals';
 
-module.exports = [
+export default [
   {
     ignores: ['node_modules/**', 'dist/**', 'build/**'],
   },
@@ -10,7 +10,7 @@ module.exports = [
     files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 'latest',
-      sourceType: 'script',
+      sourceType: 'module',
       globals: {
         ...globals.browser,
         ...globals.node,
